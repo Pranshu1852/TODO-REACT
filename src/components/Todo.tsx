edfed0af6b2260fa955c57dfc22e81a13e3adb81
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-05-14 11:48:49 +0530

    Add: Create todo component UI

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,22 +1,100 @@
-function Todo() {
+export enum PriorityType {
+  HIGH = "High",
+  MEDIUM = "Medium",
+  LOW = "Low",
+}
+
+export enum StatusType {
+  NOTSELECTED = "Not Started",
+  INPROGRESS = "In Progress",
+  COMPLETED = "Completed",
+}
+
+interface TodoProps {
+  title: string;
+  description: string;
+  priority: PriorityType;
+  status: StatusType;
+  date: Date;
+}
+
+function Todo({ title, description, priority, status, date }: TodoProps) {
+  const formattedDate = (function formatDate(date: Date) {
+    const yyyy = date.getFullYear();
+    let mm: string | number = date.getMonth() + 1;
+    let dd: string | number = date.getDate();
+
+    if (dd < 10) {
+      dd = "0" + dd;
+    }
+    if (mm < 10) {
+      mm = "0" + mm;
+    }
+
+    return dd + "/" + mm + "/" + yyyy;
+  })(date);
+
+  const priorityColor = (function getPriorityColor(priority: PriorityType) {
+    switch (priority) {
+      case PriorityType.HIGH: {
+        return "text-red-600";
+        break;
+      }
+      case PriorityType.MEDIUM: {
+        return "text-orange-600";
+        break;
+      }
+      case PriorityType.LOW: {
+        return "text-yellow-600";
+        break;
+      }
+      default: {
+        return "";
+        break;
+      }
+    }
+
+    return "";
+  })(priority);
+
+  const statusColor = (function getPriorityColor(status: StatusType) {
+    switch (status) {
+      case StatusType.NOTSELECTED: {
+        return "text-purple-600";
+        break;
+      }
+      case StatusType.INPROGRESS: {
+        return "text-blue-600";
+        break;
+      }
+      case StatusType.COMPLETED: {
+        return "text-green-600";
+        break;
+      }
+      default: {
+        return "";
+        break;
+      }
+    }
+
+    return "";
+  })(status);
+
   return (
     <div className="flex flex-col gap-5 border-2 border-black p-5 rounded-lg bg-todo bg-cover">
-      <h3>Todo Title</h3>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda vero
-        eaque possimus? Accusantium facere aspernatur vero iusto cumque iste
-        corporis, laboriosam similique, omnis nam quas quam eos officia ratione
-        aut?
-      </p>
-      <div className="flex flex-row gap-2 justify-between">
+      <h3 className="text-2xl font-semibold font-[Tagesschrift] truncate">
+        {title}
+      </h3>
+      <p className="truncate">{description}</p>
+      <div className="flex flex-row gap-2 justify-between text-sm md:text-base font-medium">
         <p>
-          Priority: <span>High</span>
+          Priority: <span className={`${priorityColor}`}>{priority}</span>
         </p>
         <p>
-          Status: <span>Not Started</span>
+          Status: <span className={`${statusColor}`}>{status}</span>
         </p>
         <p>
-          Created at: <span>14/05/2025</span>
+          Created at: <span>{formattedDate}</span>
         </p>
       </div>
     </div>
--- src/features/Todos/pages/TodosPage.tsx
+++ src/features/Todos/pages/TodosPage.tsx
@@ -1,18 +1,46 @@
 import AddTaskIcon from "@mui/icons-material/AddTask";
 import { Link } from "react-router-dom";
-import Todo from "../../../components/Todo";
+import Todo, { PriorityType, StatusType } from "../../../components/Todo";
 import SearchBar from "../components/SearchBar";
+import { useTranslation } from "react-i18next";
 
 function TodosPage() {
+  const { t } = useTranslation();
+
   return (
     <div className="flex flex-col gap-10">
       <div className="flex flex-row border-2 border-black p-5 rounded-lg">
         <SearchBar />
       </div>
+
       <div className="grid grid-cols-autofill-250 sm:grid-cols-autofill-500 gap-5">
-        <Todo />
-        <Todo />
-        <Todo />
+        <Todo
+          title={t("Todo Title")}
+          description={t(
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum veritatis rerum assumenda illum libero eum eveniet, quaerat doloribus nostrum totam possimus fuga in blanditiis dolor adipisci aliquam odio veniam natus.",
+          )}
+          priority={PriorityType.HIGH}
+          status={StatusType.COMPLETED}
+          date={new Date()}
+        />
+        <Todo
+          title={t("Todo Title")}
+          description={t(
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum veritatis rerum assumenda illum libero eum eveniet, quaerat doloribus nostrum totam possimus fuga in blanditiis dolor adipisci aliquam odio veniam natus.",
+          )}
+          priority={PriorityType.MEDIUM}
+          status={StatusType.INPROGRESS}
+          date={new Date()}
+        />
+        <Todo
+          title={t("Todo Title")}
+          description={t(
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum veritatis rerum assumenda illum libero eum eveniet, quaerat doloribus nostrum totam possimus fuga in blanditiis dolor adipisci aliquam odio veniam natus.",
+          )}
+          priority={PriorityType.LOW}
+          status={StatusType.NOTSELECTED}
+          date={new Date()}
+        />
       </div>
       <Link
         to="/addtodo"
