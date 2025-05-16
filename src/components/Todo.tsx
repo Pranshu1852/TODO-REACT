3e60c3333a0b839dde1ab2b2fa60e69da70d3e1b
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-05-16 19:53:48 +0530

    Add(feature/addtodo): Implement logic to render all todos at todos page

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,9 @@
 import { Link } from 'react-router-dom';
+import {
+  formatDate,
+  getPriorityColor,
+  getStatusColor,
+} from '../utils/todoUtils';
 
 export enum PriorityType {
   HIGH = 'High',
@@ -18,66 +23,13 @@ interface TodoProps {
   priority: PriorityType;
   status: StatusType;
   date: Date;
+  id: string;
 }
 
-function Todo({ title, description, priority, status, date }: TodoProps) {
-  const formattedDate = (function formatDate(date: Date) {
-    const yyyy = date.getFullYear();
-    let mm: string | number = date.getMonth() + 1;
-    let dd: string | number = date.getDate();
-
-    if (dd < 10) {
-      dd = '0' + dd;
-    }
-    if (mm < 10) {
-      mm = '0' + mm;
-    }
-
-    return dd + '/' + mm + '/' + yyyy;
-  })(date);
-
-  const priorityColor = (function getPriorityColor(priority: PriorityType) {
-    switch (priority) {
-      case PriorityType.HIGH: {
-        return 'text-red-600';
-      }
-      case PriorityType.MEDIUM: {
-        return 'text-orange-600';
-      }
-      case PriorityType.LOW: {
-        return 'text-yellow-600';
-      }
-      default: {
-        return '';
-      }
-    }
-  })(priority);
-
-  const statusColor = (function getPriorityColor(status: StatusType) {
-    switch (status) {
-      case StatusType.NOTSELECTED: {
-        return 'text-purple-600';
-      }
-      case StatusType.INPROGRESS: {
-        return 'text-blue-600';
-        break;
-      }
-      case StatusType.COMPLETED: {
-        return 'text-green-600';
-        break;
-      }
-      default: {
-        return '';
-        break;
-      }
-    }
-
-    return '';
-  })(status);
-
+function Todo({ title, description, priority, status, date, id }: TodoProps) {
   return (
     <Link
-      to={`123`}
+      to={id}
       className="flex flex-col gap-5 border-2 border-black p-5 rounded-lg bg-todo-100 text-black bg-cover"
     >
       <h3 className="text-2xl font-semibold font-[Tagesschrift] truncate">
@@ -86,13 +38,14 @@ function Todo({ title, description, priority, status, date }: TodoProps) {
       <p className="truncate">{description}</p>
       <div className="flex flex-row gap-2 justify-between text-sm md:text-base font-medium">
         <p>
-          Priority: <span className={`${priorityColor}`}>{priority}</span>
+          Priority:{' '}
+          <span className={`${getPriorityColor(priority)}`}>{priority}</span>
         </p>
         <p>
-          Status: <span className={`${statusColor}`}>{status}</span>
+          Status: <span className={`${getStatusColor(status)}`}>{status}</span>
         </p>
         <p>
-          Created at: <span>{formattedDate}</span>
+          Created at: <span>{formatDate(date)}</span>
         </p>
       </div>
     </Link>
--- src/features/Todos/pages/TodoDetailPage.tsx
+++ src/features/Todos/pages/TodoDetailPage.tsx
@@ -1,55 +1,69 @@
-import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { TodoContext, type Todo } from "../context/TodoContext";
-import { formatDate, getPriorityColor, getStatusColor } from "../../../utils/todoUtils";
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { TodoContext, type Todo } from '../context/TodoContext';
+import {
+  formatDate,
+  getPriorityColor,
+  getStatusColor,
+} from '../../../utils/todoUtils';
 
 function TodoDetailPage() {
-  const { id } =useParams();
-  const todoContext=useContext(TodoContext);
-  const [todoData,setTodoData]=useState<Todo|undefined>(undefined);
+  const { id } = useParams();
+  const todoContext = useContext(TodoContext);
+  const [todoData, setTodoData] = useState<Todo | undefined>(undefined);
 
-  if(!id){
-    return <h2>Please Provide valid id.</h2>
-  }
-
-  if(!todoContext){
-    return;
-  }
-
-  useEffect(()=>{
+  useEffect(() => {
     function findTodo(todoArray: Array<Todo>, id: string) {
-      const todo=todoArray.find((element)=>{
-          return element.id===id;
-      })
-      
+      const todo = todoArray.find((element) => {
+        return element.id === id;
+      });
+
       return todo;
     }
 
-    const todo=findTodo(todoContext.state.todoArray, id);
-    setTodoData(todo);
-  },[])
+    if (!todoContext || !id) {
+      return;
+    }
 
-  if(!todoData) {
-    return <h2>Something wrong.</h2>
+    const todo = findTodo(todoContext.state.todoArray, id);
+    setTodoData(todo);
+  }, []);
+
+  if (!id) {
+    return <h2>Please Provide valid id.</h2>;
+  }
+
+  if (!todoContext) {
+    return;
+  }
+
+  if (!todoData) {
+    return <h2>Something wrong.</h2>;
   }
 
   return (
     <div className="flex flex-col gap-10 border-2 border-black rounded-lg p-10 bg-todo-60 bg-cover text-black">
-      <h2 className="text-3xl font-bold font-[Tagesschrift]">{todoData.title}</h2>
+      <h2 className="text-3xl font-bold font-[Tagesschrift]">
+        {todoData.title}
+      </h2>
       <div className="flex flex-row gap-2 justify-between text-sm md:text-base font-medium">
         <p>
-          Priority: <span className={`${getPriorityColor(todoData.priority)}`}>{todoData.priority}</span>
+          Priority:{' '}
+          <span className={`${getPriorityColor(todoData.priority)}`}>
+            {todoData.priority}
+          </span>
         </p>
         <p>
-          Status: <span className={`${getStatusColor(todoData.status)}`}>{todoData.status}</span>
+          Status:{' '}
+          <span className={`${getStatusColor(todoData.status)}`}>
+            {todoData.status}
+          </span>
         </p>
         <p>
           Created at: <span>{formatDate(new Date(todoData.created_at))}</span>
         </p>
       </div>
-      <p className="text-xl">
-        {todoData.description}
-      </p>
+      <p className="text-xl">{todoData.description}</p>
       <div className="flex flex-row gap-5 justify-between">
         <button className="text-lg font-semibold py-2 px-4 border-2 border-black rounded-lg hover:bg-black hover:text-white transition-all">
           Edit
